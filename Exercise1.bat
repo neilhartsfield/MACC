5d0fa1f3fb303c3ba77be553592f1e99f738ddb3
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2022-05-18 15:36:55 -0500

    Update Exercise1.bat

--- Exercise1.bat
+++ Exercise1.bat
@@ -1,6 +1,6 @@
 @echo off
 
 echo @echo off > .\Exercise0.bat
-echo *** SUCCESS *** >> .\Exercise0.bat
+echo echo *** SUCCESS *** >> .\Exercise0.bat
 echo echo Exercise0.bat has been succesfully changed. >> .\Exercise0.bat
 echo pause >> .\Exercise0.bat && .\Exercise0.bat
